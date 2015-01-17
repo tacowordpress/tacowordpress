f577ff3d555469bf658665e1d5d5dc30ea483516
Brian Haveri <brian.haveri@gmail.com>
2015-01-17 07:33:43 -0700

    frontend - start adding js

--- src/Loader.php
+++ src/Loader.php
@@ -7,6 +7,7 @@ class Loader {
     add_action('init', '\Taco\Post\Loader::loadAll');
     add_action('init', '\Taco\Post\Loader::registerTaxonomies');
     add_action('init', '\Taco\Term\Loader::loadAll');
+    add_action('shutdown', '\Taco\Frontend\Loader::addToHTML');
     return true;
   }
 }
--- tests/wp-content/plugins/taco_hot_sauce/taco_hot_sauce.php
+++ tests/wp-content/plugins/taco_hot_sauce/taco_hot_sauce.php
@@ -10,10 +10,12 @@ class HotSauce extends \Taco\Post {
   public function getFields() {
     return array(
       'scovilles'=>array('type'=>'number'),
+      'image_path'=>array('type'=>'image'),
+      'file_path'=>array('type'=>'file'),
     );
   }
 
   public function getSupports() {
-    return array('title');
+    return array('title', 'editor');
   }
 }
